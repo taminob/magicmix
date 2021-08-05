f19bb7cb946305b4c924b5e772d0ba1f5adf98c4
Tamino Bauknecht <dev@tb6.eu>
2021-08-05 23:05:50 +0200

    more performant (still ugly) fire scene; vladimir dialogue updated to newer system

--- characters/dialogue/vladimir/dialogue.gd
+++ characters/dialogue/vladimir/dialogue.gd
@@ -1,7 +1,7 @@
 extends abstract_dialogue
 
 func _init_statements():
-	_start_statements["hans"] = statement.new(_arrest_sentence())
+	_start_statements["hans"] = want_to_arrest()
 #	statements = [
 #		statement.new(_arrest_sentences()[randi() % _arrest_sentences().size()], [
 #			answer.new("I surrender!", funcref(self, "_no_effect")), 
@@ -15,6 +15,20 @@ func _init_statements():
 #			answer.new("Sure", 2, funcref(self, "_end_dialogue")),
 #			answer.new("Ha, tricked you! Only one of us will survive the day.", 0, funcref(self, "_end_dialogue"))])
 #	]
+
+func want_to_arrest() -> Array:
+	var statement_name: String = "some"
+	_statements[statement_name] = _create_statements_from_dict({
+		"start": {
+			"say": _arrest_sentence(),
+			"answers": [
+				{
+					"say": ""
+				}
+			]
+		}
+	}, [statement_name])
+	return [statement_name, "start"]
 
 func _arrest_sentence() -> String:
 	return util.random_element([
