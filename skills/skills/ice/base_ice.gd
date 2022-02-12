26c27d815e35afebe14bd3c4733405d0c7c39784
Tamino Bauknecht <dev@tb6.eu>
2022-02-12 23:05:12 +0100

    added ice_push spell to base_ice skill

--- skills/skills/ice/base_ice.gd
+++ skills/skills/ice/base_ice.gd
@@ -24,11 +24,14 @@ func on_allocated(pawn: KinematicBody):
 	pawn.inventory.add_spell(ice_wave_spell.id())
 # warning-ignore:return_value_discarded
 	pawn.inventory.add_spell(ice_ride_spell.id())
+# warning-ignore:return_value_discarded
+	pawn.inventory.add_spell(ice_push_spell.id())
 
 func on_retracted(pawn: KinematicBody):
 	pawn.inventory.remove_spell(ice_ball_spell.id())
 	pawn.inventory.remove_spell(ice_wave_spell.id())
 	pawn.inventory.remove_spell(ice_ride_spell.id())
+	pawn.inventory.remove_spell(ice_push_spell.id())
 
 func icon() -> Resource:
 	return load(SKILL_ICONS_PATH + "blue_star-512.png")
