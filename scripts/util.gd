40417e9516331dff0d22831662d348e11c5b753a
Tamino Bauknecht <dev@tb6.eu>
2022-02-13 20:26:07 +0100

    added util absolute_path (might be removed again)

--- scripts/util.gd
+++ scripts/util.gd
@@ -8,3 +8,7 @@ func max(first: float, second: float, third: float) -> float:
 
 func random_element(array: Array):
 	return array[randi() % array.size()]
+
+# script can be obtained via get_script()
+func absolute_path(script: Resource, relative_path: String) -> String:
+	return script.resource_path.get_base_dir() + "/" + relative_path
